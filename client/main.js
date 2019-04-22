2951c6fe1e670b7bb34fd4f1b87c523a9cc687a9
ruel <ruelpaquera@gmail.com>
2019-04-22 13:20:04 +0800

    aw

--- client/main.js
+++ client/main.js
@@ -25,43 +25,73 @@ Template.hello.events({
   },
 });
   
-function setup(device) {
+async function setup(device) {
 
-  return device.open()
+  return await device.open()
       .then(() => device.selectConfiguration(1))
-      .then(() => device.claimInterface(0));
+      .then(() => device.claimInterface(0))
+      // .then(() => device.controlTransferOut({
+      //   requestType: 'class',
+      //   recipient: 'interface',
+      //   request: 0x22,
+      //   value: 0x01,
+      //   index: 0x02})) // Ready to receive data
+      // .then(() => device.transferIn(5, 64)) // Waiting for 64 bytes of data from endpoint #5.
+      // .then(result => {
+      //   let decoder = new TextDecoder();
+      //   console.log('Received: ' + decoder.decode(result.data));
+      // })
   // device.instance();
 }
 async function print() { 
-  navigator.usb.claimInterface()
+  // navigator.usb.claimInterface()
   // var string = document.getElementById("printContent").value + "\n";
   // var encoder = new TextEncoder();
   // var data = encoder.encode("string");
   // device.transferOut(1, data)
   // .catch(error => { console.log(error); })
-
+    // console.log(device.configuration);
   // let loop = 0;
   // setInterval(()=>{
   //   loop++;
   //   console.log("call device");
   // },800);
-  
+    /*
+   .then(() => device.isochronousTransferIn({
+    requestType: 'vendor',
+    recipient: 'interface',
+    request: 0x01,  // vendor-specific request: enable channels
+    value: 0x0013,  // 0b00010011 (channels 1, 2 and 5)
+    index: 0x0000   // Interface 1 is the recipient
+  })) // Ready to receive data 
+  */
   // await device.transferOut(1, 0x05ba);
-  // let receivedData2 = await device.controlTransferOut({
-  //     requestType: 'vendor',
-  //     recipient: 'interface',
-  //     request: 0x01,  // vendor-specific request: enable channels
-  //     value: 0x0013,  // 0b00010011 (channels 1, 2 and 5)
-  //     index: 0x0001   // Interface 1 is the recipient
-  // });
+  var buffer = new ArrayBuffer(1);
+  await device.open()
+  .then(() => device.selectConfiguration(1))
+  .then(() => device.claimInterface(0))
+  .then(() => device.isochronousTransferOut(0x0000,(data)=>{
+
+  },6)) // Ready to receive data
+  // .then(() => device.transferIn(2, 8)) // Waiting for 64 bytes of data from endpoint #5.
+  .then(result => {
+    let decoder = new TextDecoder();
+    console.log('Received: ' ,result);
+    console.log('Received: ' + decoder.decode(result.data));
+  });
 
   // let receivedData = await device.transferIn(1, 6);
   // console.log("receivedData",receivedData);
 }
 function connectAndPrint() {
   if (device == null) { 
-    navigator.usb.requestDevice({ filters: [{ vendorId: 0x05ba }] })
+    navigator.usb.requestDevice({ filters: [{ 
+      vendorId: 0x05ba,
+      productId: 0x000a
+    }]})
     .then(selectedDevice => {
+      console.log(selectedDevice.productName);      // "Arduino Micro"
+      console.log(selectedDevice.manufacturerName); // "Arduino LLC"
       device = selectedDevice; 
       return setup(device);
     })
@@ -69,14 +99,16 @@ function connectAndPrint() {
     .catch(error => { console.log(error); })
   }
   else 
-    print();
+    print(); 
 }
 navigator.usb.getDevices()
 .then(devices => {
   if (devices.length > 0) {
     device = devices[0];
-    // console.log(device);
+    console.log(device);
     return setup(device);
+  }else {
+    connectAndPrint();
   }
 })
 .catch(error => { console.log(error); });
@@ -91,7 +123,7 @@ navigator.usb.addEventListener('disconnect', event => {
   console.log("event disconnect" ,event);
 }); 
 
-navigator.usb.addEventListener('click', event => {
+navigator.usb.addEventListener('Touch', event => {
   // event.device will bring the connected device
   console.log("event connect" ,event);
 });
