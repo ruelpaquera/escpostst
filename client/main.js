6715da1064386f5f25e21d9cf4cba617ffa21e25
jhonortes <jhonortes@yahoo.com>
2019-04-22 13:16:11 +0800

    aw

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';   
+// import { USBPrinter, NetPrinter, BLEPrinter } from 'react-native-printer';
 // var usb = require('usb').usb;
 // const path = require("path");
 // const exec = require("child_process").exec;
@@ -20,33 +21,85 @@ Template.hello.helpers({
 Template.hello.events({
   'click button'(event, instance) {
     connectAndPrint();
+    // var encoder = new TextEncoder();
+    // var data = encoder.encode("tsting");
+    // console.log(data);
+    // Meteor.call('tests',data);
     // increment the counter when button is clicked
+    // myPrint("aw");
+
+  // console.log("================================")
+  // var  VENDOR_ID = 0x04a9;  
+  // var  PRODUCT_ID = 0x10d3;  
+  // var DEVICE_INFO = { "vendorId": VENDOR_ID, "productId": PRODUCT_ID };
+  // navigator.usb.requestDevice({ filters: [{ vendorId: 0x04a9 }] })
+  // .then(selectedDevice => {
+  //   console.log("selectedDevice",selectedDevice);
+  //   device = selectedDevice; 
+  //   return setup(device);
+  // })
+  // .then(() => print())
+  // .catch(error => { 
+  //   console.log("error",error); })
+
     instance.counter.set(instance.counter.get() + 1);
   },
 });
-  
-function setup(device) {
-
+function myPrint(data) 
+{
+    var testPage = window.open('', 'Test Page',  'height=500,width=500');
+    testPage.document.write('<html><head><title>Test Page</title>');
+    testPage.document.write('</head><body >');
+    testPage.document.write(data);
+    testPage.document.write('</body></html>');
+    testPage.document.close(); 
+    testPage.focus(); 
+    testPage.print();
+    testPage.close();
+    return ;
+}
+function setup(device) { 
   return device.open()
       .then(() => device.selectConfiguration(1))
-      .then(() => device.claimInterface(0));
+      .then(() => device.claimInterface(0))
+      .then(() => device.transferIn(5, 64)) // Waiting for 64 bytes of data from endpoint #5.
+      .then(result => {
+        let decoder = new TextDecoder();
+        console.log('Received: ' + decoder.decode(result.data));
+      })
+      .catch((evt) => {         
+        console.log(evt.code); 
+        console.log(evt.message); 
+        console.log(evt.name);  
+      });
   // device.instance();
 }
 async function print() { 
-  navigator.usb.claimInterface()
+  // navigator.usb.claimInterface(0);
   // var string = document.getElementById("printContent").value + "\n";
-  // var encoder = new TextEncoder();
-  // var data = encoder.encode("string");
+  var encoder = new TextEncoder();
+  var data = encoder.encode("string");
   // device.transferOut(1, data)
-  // .catch(error => { console.log(error); })
+  // .catch((evt) => { console.log(evt); })
 
   // let loop = 0;
   // setInterval(()=>{
   //   loop++;
   //   console.log("call device");
-  // },800);
-  
-  // await device.transferOut(1, 0x05ba);
+  // },800);0x05ba
+  await device.open()
+  .then(() => device.selectConfiguration(1))
+  .then(() => device.claimInterface(0))
+  .catch((evt) => { 
+    console.log(evt.code); 
+    console.log(evt.message); 
+    console.log(evt.name);  
+  });;
+  await device.transferOut(1, data).catch((evt) => { 
+    console.log(evt.code); 
+    console.log(evt.message); 
+    console.log(evt.name);  
+  });
   // let receivedData2 = await device.controlTransferOut({
   //     requestType: 'vendor',
   //     recipient: 'interface',
@@ -62,36 +115,100 @@ function connectAndPrint() {
   if (device == null) { 
     navigator.usb.requestDevice({ filters: [{ vendorId: 0x05ba }] })
     .then(selectedDevice => {
+      console.log("selectedDevice",selectedDevice);
       device = selectedDevice; 
       return setup(device);
     })
     .then(() => print())
-    .catch(error => { console.log(error); })
+    .catch(error => { 
+      console.log("error",error); })
   }
   else 
     print();
 }
-navigator.usb.getDevices()
-.then(devices => {
-  if (devices.length > 0) {
-    device = devices[0];
-    // console.log(device);
-    return setup(device);
-  }
-})
-.catch(error => { console.log(error); });
+async function loads(){
+  navigator.usb.getDevices()
+  .then(devices => {
+    console.log(devices);
+    if (devices.length > 0) {
+      device = devices[0];
+      // console.log(device);
+      return setup(device);
+    }
+  })
+  .catch(error => { console.log(error); });
+}
+loads();
 
-navigator.usb.addEventListener('connect', event => {
-  // event.device will bring the connected device
-  console.log("event connect" ,event);
-});
+// navigator.usb.addEventListener('connect', event => {
+//   // event.device will bring the connected device
+//   console.log("event connect" ,event);
+// });
 
-navigator.usb.addEventListener('disconnect', event => {
-  // event.device will bring the disconnected device
-  console.log("event disconnect" ,event);
-}); 
+// navigator.usb.addEventListener('disconnect', event => {
+//   // event.device will bring the disconnected device
+//   console.log("event disconnect" ,event);
+// }); 
 
-navigator.usb.addEventListener('click', event => {
-  // event.device will bring the connected device
-  console.log("event connect" ,event);
-});
+// navigator.usb.addEventListener('click', event => {
+//   // event.device will bring the connected device
+//   console.log("event connect" ,event);
+// });
+
+
+
+
+
+ 
+// 	chrome.hid.getDevices(DEVICE_INFO, function(devices) {
+// 		 console.log('Device Length: '+devices.length);
+// 		if (!devices || !devices.length) {
+// 		  console.log('device not found');
+// 		  return;
+// 		}
+// 		console.log('Found device: ' + devices[0].deviceId);
+// 		myHidDevice = devices[0].deviceId;
+       	
+// 		console.log('ProductId: '+devices[0].productId.toString(16));
+// 		console.log('VendorId: '+devices[0].vendorId.toString(16));
+		 
+// 	   chrome.hid.connect(myHidDevice, function(connection) {
+			
+			
+// 		console.log('Connected to the HID device!');
+// 		console.log(connection);
+// 		connectionId = connection.connectionId;
+		
+		
+// 		var arrayLength = 24;
+// 		var message = new Uint8Array(arrayLength);
+
+// 		message[0] = 0x05; 
+// 		message[1] = 0x2F; 
+// 		message[2] = 0x43; 
+// 		message[3] = 0x4C; 
+// 		message[4] = 0x3C; 
+// 		message[5] = 0x2F; 
+// 		for (var i = 6; i < arrayLength; i++) {
+// 		message[i] = 0x00;
+// 		}
+
+// 		var TransferData = {
+// 		"requestType": "class",
+// 		"recipient": "interface",
+// 		"direction": "out",
+// 		"request": 0x09,
+// 		"value": 0x0300,
+// 		"index": 0,
+// 		"data": message.buffer
+// 		};
+
+		
+		
+		
+//         chrome.hid.send(connectionId,0, message.buffer, function() {
+// 			console.log("Info received by device");
+// 		}); 
+// 	   });
+// 	});
+
--- server/main.js
+++ server/main.js
@@ -1,46 +1,185 @@
 import { Meteor } from 'meteor/meteor';
+import Future from 'fibers/future';
 
-// var USB = require("webusb").USB;
-
-const path = require("path");
-const exec = require("child_process").exec;
-const usb = require('webusb').usb;
-
-function deviceFound(device) {
-  console.log(`Device '${device.productName || device.serialNumber}' connected`);
-  if (device.url) openUrl(device.url);
-}
-
-function openUrl(url) {
-  console.log(`Opening url: ${url}`);
-
-  var cmd = path.join(__dirname, "xdg-open");
-  if (process.platform === "darwin") cmd = "open";
-  else if (process.platform === "win32") cmd = `start ""`;
-
-  exec(`${cmd} ${url}`);
-}
-
-console.log("Searching for Web USB devices...");
-
-// usb.requestDevice({
-//   filters: [{vendorId: 0x05ba}]
-// })
-// .then(device => deviceFound(device))
-// .catch(error => {
-//   console.log(error.message);
-//   process.exit();
-// });
-
-// usb.addEventListener("connect", (device)=>{
-//   console.log(`Device '${device.productName || device.serialNumber}' connected`);
-//   if (device.url) openUrl(device.url);
-// });
-
-// usb.addEventListener("disconnect", device => {
-//   console.log(`Device '${device.productName || device.serialNumber}' disconnected`);
-// });
+// import { ReactNativePrinter } from 'react-native-printer';
  
+// ReactNativePrinter.print('192.168.31.242', 9100, '<CB>这是一个标题</CB>');
+// const LinePrinter = require("lineprinter");
+// import TransportWebUSB from "@ledgerhq/hw-transport-webusb";
+// const ipp = require('ipp-encoder');
+// const C = ipp.CONSTANTS;
+
+// const printer = require("printer-lp");
+// const printer = require('node-native-printer');
+// var USB = require("webusb").USB;
+// var Printer = require('node-printer');
+// var options = {
+//     media: 'Custom.200x600mm',
+//     n: 3
+// };
+// Get available printers list
+// const path = require("path");
+// const exec = require("child_process").exec;
+// const usb = require('webusb').usb;
+var USB = require("webusb").USB;
+let device; 
+Meteor.methods({
+  'tests': async function(data){  
+    var fut = new Future();
+    console.log("called");
+
+    // var decoded = ipp.request.decode(data);
+
+    // var response = {
+    //   version: {
+    //     major: 1,
+    //     minor: 1
+    //   },
+    //   operationId: 0x02,
+    //   requestId: 1,
+    //   groups: [
+    //     { tag: C.OPERATION_ATTRIBUTES_TAG, attributes: [
+    //       { tag: 0x47, name: 'attributes-charset', value: ['utf-8'] },
+    //       { tag: 0x48, name: 'attributes-natural-language', value: ['en-us'] },
+    //       { tag: 0x45, name: 'printer-uri', value: ['ipp://TISAY-PC/'] },
+    //       { tag: 0x42, name: 'job-name', value: ['foobar'] },
+    //       { tag: 0x22, name: 'ipp-attribute-fidelity', value: [true] }
+    //     ] },
+    //     { tag: C.JOB_ATTRIBUTES_TAG, attributes: [
+    //       { tag: 0x21, name: 'copies', value: [20] },
+    //       { tag: 0x44, name: 'sides', value: ['two-sided-long-edge'] }
+    //     ] }
+    //   ]
+    // }
+    // ipp.response.encode(response)
+
+
+    // var options = {
+    //     media: 'Custom.200x600mm', // Custom paper size
+    //     destination: "Cannon_iP2700", // The printer name
+    //     n: 3 // Number of copies
+    // };
+     
+    // var text = "print text directly, when needed: e.g. barcode printers";
+    // var file = "package.json";
+     
+    // var jobText = printer.printText(text, options, "text_demo");
+    // var jobFile = printer.printFile(file, options, "file_demo");
+     
+    // var onJobEnd = function () {
+    //     console.log(this.identifier + ", job send to printer queue");
+    // };
+     
+    // var onJobError = function (message) {
+    //     console.log(this.identifier + ", error: " + message);
+    // };
+     
+    // jobText.on("end", onJobEnd);
+    // jobText.on("error", onJobError);
+     
+    // jobFile.on("end", onJobEnd);
+    // jobFile.on("error", onJobError);
+    // connectAndPrint();
+// let p = Printer.list();
+
+// var printer = new Printer('usbprint');
+// console.log(p);
+
+    fut.return(null); 
+    return fut.wait();
+  }
+});
+function setup(device) {
+  console.log('device opend');
+  return device.open() 
+  .then(() => device.selectConfiguration(1))
+  .then(() => device.claimInterface(device.configuration.interfaces[0].interfaceNumber))
+}
+function print() {
+  console.log('print'); 
+  // var string = document.getElementById("printContent").value + "\n";
+  var encoder = new TextEncoder();
+  var data = encoder.encode("string");
+  device.transferOut(1, data)
+  .catch(error => { console.log(error); })
+}
+function connectAndPrint() {
+  if (device == null) {
+    usb.requestDevice({ filters: [{ vendorId: 0x04a9 }] })
+    .then(selectedDevice => {
+      device = selectedDevice;
+      // console.log(device); 
+      return setup(device);
+    })
+    .then((obj) =>{
+      console.log("obj",obj)
+      // print()
+    })
+    .catch(error => { console.log("error",error); })
+  }
+  else
+    print();
+}
+async function loads(){
+
+  await usb.getDevices()
+  .then(devices => {
+    // console.log(devices);
+    if (devices.length > 0) {
+      console.log("connected to vendor");
+      device = devices[0];
+      return setup(device);
+    } else {
+      console.log("connecting to vendor");
+      usb.requestDevice({ filters: [{ vendorId: 0x05ba }] })
+      .then(selectedDevice => {
+        device = selectedDevice;
+        console.log(device); 
+        return setup(device);
+      })  
+      .then((obj) =>{
+        console.log("obj",obj)  
+        // print()
+      })
+      .catch(error => { console.log("error",error); })
+    }
+  })
+  .catch(error => { console.log(error); });
+
+}
+// loads();
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+
+function handleDevicesFound(devices, selectFn) {
+  // If one of the devices can be automatically selected, you can return it
+  for (var i = 0; i < devices.length; i++) {
+    console.log(devices[i].productName);
+      // if (devices[i].productName === "myName") return devices[i];
+  }
+
+  // Otherwise store the selectFn somewhere and execute it later with a device to select it
+}
+
+// var usb = new USB({
+//   devicesFound: handleDevicesFound
+// });
+
+// usb.requestDevice({
+//   filters: [{vendorId: 0x04a9}]
+// })
+// .then(device => {
+//   console.log(device);
+// });
+
+
+// printers();
+async function printers(){
+  
+// const printer = await LinePrinter.auto();
+// await printer.println("Hello, World!");
+console.log(await LinePrinter.list());
+
+}
